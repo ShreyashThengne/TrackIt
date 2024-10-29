b8cc1e5c4a8d46f362b9fed44f481859b3dee8b5
Shreyash Thengne <shreyashthengne@gmail.com>
2024-10-29 22:42:38 +0530

    just simple edit

--- sam.py
+++
@@ -1,9 +0,0 @@
-c = r"."
-import os
-# for (root,dirs,files) in os.walk('.', topdown=True):
-#     print (root)
-#     print (dirs)
-#     print (files)
-#     print("\n\n\n")
-for i in os.scandir('.'):
-    print(type(i))
